8cc346b44546776116a7fb5735fd87c26bd40cee
dkhurlop <denisekhurlopian@gmail.com>
2023-06-06 16:05:58 +0200

    vaciar js

--- functions.js
+++ functions.js
@@ -1,11 +1,1 @@
-let url = 'https://uselessfacts.jsph.pl/api/v2/facts/random'
-fetch(url)
-    .then(response => response.json())
-    .then(data => mostrarData(data))
-    .catch(error => console.log(error))
 
-const mostrarData = (data) => {
-    let element = document.getElementById('element');
-    element.innerHTML = `<p>${data.name}`
-    console.log(data)
-}
