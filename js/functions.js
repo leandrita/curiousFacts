49f9c322174e7099c3f78ca05bc04b13f37fd32c
leandrita <lea.bujh@gmail.com>
2023-06-09 09:35:45 +0200

    estilos version desktop

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,43 @@
-let url = 'https://uselessfacts.jsph.pl/api/v2/facts/random'
-fetch(url)
+const fetchData = () => {
+  let url = 'https://uselessfacts.jsph.pl/api/v2/facts/random';
+
+  fetch(url)
     .then(response => response.json())
-    .then(data => mostrarData(data))
-    .catch(error => console.log(error))
+    .then(data => {
+      mostrarData(data);
+      console.log(data);
+    })
+    .catch(error => console.log(error));
+}
+
+const addToFavourites = (text) => {
+  let favouriteList = [];
+
+  if (localStorage.getItem('favouriteList')) {
+    favouriteList = JSON.parse(localStorage.getItem('favouriteList'));
+  }
+
+  favouriteList.push(text);
+
+  localStorage.setItem('favouriteList', JSON.stringify(favouriteList));
+
+  console.log('Added to favourites');
+}
 
 const mostrarData = (data) => {
-    let element = document.getElementById('element');
-    element.innerHTML = `<p>${data.text}</p>`;
-    // console.log(data)
+  let randomText = document.getElementById('randomText');
+  randomText.innerHTML = `<p>${data.text}</p>`;
+
+  let favouriteBtn = document.createElement('button');
+  favouriteBtn.textContent = 'Add to favourites';
+  favouriteBtn.addEventListener('click', () => addToFavourites(data.text));
+
+  randomText.appendChild(favouriteBtn);
+
+  console.log(data);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const randomBtn = document.getElementById('randomBtn');
+  randomBtn.addEventListener('click', fetchData);
+});
