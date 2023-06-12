cad1b2f1109a5802e553454130c68676913eb49c
dkhurlop <denisekhurlopian@gmail.com>
2023-06-12 12:34:01 +0200

    changes test

--- __test__/button.test.js
+++ __test__/button.test.js
@@ -1,1 +1,8 @@
 const { getFact, addToFavorites, displayFact, createFactElement, fetchFact } = require("../src/functions.js");
+function createMockElement() {
+  return {
+    innerHTML: "",
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+  };
+}
